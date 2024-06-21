4108299b876215eb87e08d5d01f244584ce69c0a
Mahdi <mah454@mail.moke.ir>
2024-06-22 02:30:20 +0330

    change ModInfo field name

--- src/main/java/ir/moke/jsysbox/system/ModInfo.java
+++ src/main/java/ir/moke/jsysbox/system/ModInfo.java
@@ -1,4 +1,4 @@
 package ir.moke.jsysbox.system;
 
-public record ModInfo(String name,long size,int usedBy,String dependencies) {
+public record ModInfo(String name,long size,int usedBy,String usedByModules) {
 }
