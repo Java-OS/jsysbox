b614129e49f58fe85ba778b80b162bbacc86391a
Mahdi <mah454@mail.moke.ir>
2024-11-14 22:56:46 +0330

    Improvement build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ sudo rm -rf target/ /tmp/jni/ src/main/clib/lib/ src/main/clib/build/
 mvn dependency:go-offline
 
 if [[ ! -z ${EXEC_UNIT_TEST} ]] && [[ ${EXEC_UNIT_TEST} -eq "1" ]] ; then 
-  mvn clean compile package -DskipTests
-else 
   mvn clean compile package 
+else 
+  mvn clean compile package -DskipTests
 fi 
 sudo chown -R $USER:$USER $HOME/.m2 
 
