46f9a86a270816bec78df90190e09321223fe67a
Mahdi <mah454@mail.moke.ir>
2024-11-14 16:40:07 +0330

    fix script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd $(dirname $0) # this line used inside container
 sudo rm -rf target/ /tmp/jni/ src/main/clib/lib/ src/main/clib/build/ 
 
 mvn dependency:go-offline
-mvn clean compile package -DskipTests 
+mvn clean compile package
 
 sudo chown -R $USER:$USER $HOME/.m2 
 
--- build_with_docker.sh
+++ build_with_docker.sh
@@ -3,6 +3,8 @@
 docker run -it --rm --name=build-jsysbox \
   -v $PWD:/opt/jsysbox \
   -v $HOME/.m2:/root/.m2 \
-  --cap-add=NET_ADMIN \
-  --cap-add=NET_RAW \
+  --privileged \
   mah454/josb:latest /opt/jsysbox/build.sh
+
+  # --cap-add=NET_ADMIN \
+  # --cap-add=NET_RAW \
