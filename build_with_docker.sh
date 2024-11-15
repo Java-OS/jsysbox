5801ea270369c2d532d232d6dc7218f8c01cedc5
Mahdi <mah454@mail.moke.ir>
2024-11-15 15:15:10 +0330

    Improvement Script

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -4,6 +4,7 @@ docker run -it --rm --name=build-jsysbox \
   -v $PWD:/opt/jsysbox \
   -v $HOME/.m2:/root/.m2 \
   --privileged \
+  -e EXEC_UNIT_TEST=${EXEC_UNIT_TEST} \
   mah454/josb:latest /opt/jsysbox/build.sh
 
   # --cap-add=NET_ADMIN \
