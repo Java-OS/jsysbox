3846f6ca60f302bbc33b3a1cc2c2ec2bd33d062d
Mahdi <mah454@mail.moke.ir>
2025-03-28 01:02:05 +0330

    Change CpuStat.java

--- src/main/java/ir/moke/jsysbox/system/CpuStat.java
+++ src/main/java/ir/moke/jsysbox/system/CpuStat.java
@@ -12,11 +12,11 @@ public record CpuStat(String core,
                       long guest,
                       long guestNice) {
 
-    public double usage() {
-        long total = user + nice + system + idle + ioWait + irq + softIrq + steal;
-        long idle_total = idle + ioWait;
-        long used = total - idle_total;
-        if (total == 0) return 0;
-        return (double) (100 * used / total);
+    public double total() {
+        return user + nice + system + idle + ioWait + irq + softIrq + steal;
+    }
+
+    public double totalIdle() {
+        return idle + ioWait;
     }
 }
