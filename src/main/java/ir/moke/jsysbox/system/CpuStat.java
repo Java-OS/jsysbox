53aeb27e29481764dd7d2f2f37eea300992b1dfc
Mahdi <mah454@mail.moke.ir>
2025-03-27 22:20:53 +0330

    return cpu usage on CpuStat model

--- src/main/java/ir/moke/jsysbox/system/CpuStat.java
+++ src/main/java/ir/moke/jsysbox/system/CpuStat.java
@@ -11,4 +11,12 @@ public record CpuStat(String core,
                       long steal,
                       long guest,
                       long guestNice) {
+
+    public double usage() {
+        long total = user + nice + system + idle + ioWait + irq + softIrq + steal;
+        long idle_total = idle + ioWait;
+        long used = total - idle_total;
+        if (total == 0) return 0;
+        return (double) (100 * used / total);
+    }
 }
