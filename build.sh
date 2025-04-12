d0167d3caf87fa0d828029a414b9bc31a988f115
Mahdi <mah454@mail.moke.ir>
2025-04-12 19:21:10 +0330

    update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ sudo rm -rf target/ /tmp/jni/ src/main/clib/lib/ src/main/clib/build/
 mvn dependency:go-offline
 
 if [[ ${EXEC_UNIT_TEST} -eq "1" ]] ; then 
-  mvn clean compile package 
+  mvn clean compile package install
 else 
-  mvn clean compile package -DskipTests
+  mvn clean compile package install -DskipTests
 fi 
 sudo chown -R $USER:$USER $HOME/.m2 .* 
 
